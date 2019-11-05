277228aaffcf7949beda60e12af9b1dbd25a3c17
VitorSDDF <vitor.sd@hotmail.com>
2019-11-05 00:32:32 -0200

    Changing project name

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adjust_test.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'performanceapp.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
